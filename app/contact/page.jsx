a0143454c9fb999c136e5a35576288fa55c11c5a
Bechir-Gh <boshreem99@gmail.com>
2025-01-14 10:13:28 +0100

    changes applied

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -155,7 +155,7 @@ const Contact = () => {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">A7ki 3ala rouhek yal boshreem</p>
+              <p className="text-white/60">For professional inquiries, please send a message</p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input 
                   type="text" 
